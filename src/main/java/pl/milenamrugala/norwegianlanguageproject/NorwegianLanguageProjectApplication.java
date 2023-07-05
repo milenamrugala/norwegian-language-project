915393e12f9fd8e8b5e46a7e349e675d2e8139a4
milenamrugala <milena.mrugala@icloud.com>
2023-07-05 17:03:06 +0200

    initial commit

---
+++ src/main/java/pl/milenamrugala/norwegianlanguageproject/NorwegianLanguageProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.milenamrugala.norwegianlanguageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorwegianLanguageProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorwegianLanguageProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/milenamrugala/norwegianlanguageproject/NorwegianLanguageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milenamrugala.norwegianlanguageproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorwegianLanguageProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
