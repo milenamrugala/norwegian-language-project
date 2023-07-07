3a4b7ea5008a71eb0e520086a07a484739b1ba3f
milenamrugala <milena.mrugala@icloud.com>
2023-07-07 19:05:13 +0200

    LevelRepository added

---
+++ src/main/java/pl/milenamrugala/norwegianlanguageproject/repository/LevelRepository.java
@@ -0,0 +1,8 @@
+package pl.milenamrugala.norwegianlanguageproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.milenamrugala.norwegianlanguageproject.model.Level;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
+
+}
