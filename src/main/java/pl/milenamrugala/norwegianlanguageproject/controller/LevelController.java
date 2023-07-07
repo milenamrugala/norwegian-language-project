46dfd68f5ee8631489487763ccac0c6d3cc00363
milenamrugala <milena.mrugala@icloud.com>
2023-07-07 19:04:51 +0200

    LevelController added

--- src/main/java/pl/milenamrugala/norwegianlanguageproject/controller/LevelController.java
+++ src/main/java/pl/milenamrugala/norwegianlanguageproject/controller/LevelController.java
@@ -1,15 +1,30 @@
 package pl.milenamrugala.norwegianlanguageproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import pl.milenamrugala.norwegianlanguageproject.service.LevelService;
 
 @Controller
+@RequestMapping(value = "/norwegian-language")
+
 public class LevelController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String home() {
-        return "in progress...";
+ private LevelService levelService;
+
+    @Autowired
+    public LevelController(LevelService levelService) {
+        super();
+        this.levelService = levelService;
     }
+
+    @GetMapping("/levels")
+    public String listLevels(Model model) {
+        model.addAttribute("levels", levelService.getAllLevels());
+        return "levels";
+    }
+
+
 }
