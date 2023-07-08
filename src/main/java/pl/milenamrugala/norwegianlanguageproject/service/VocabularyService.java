f6709bb9ab343c851f14544e72336050aa764e9d
milenamrugala <milena.mrugala@icloud.com>
2023-07-08 21:07:21 +0200

    VocabularyServices edited

--- src/main/java/pl/milenamrugala/norwegianlanguageproject/service/VocabularyService.java
+++ src/main/java/pl/milenamrugala/norwegianlanguageproject/service/VocabularyService.java
@@ -1,4 +1,18 @@
 package pl.milenamrugala.norwegianlanguageproject.service;
 
+import pl.milenamrugala.norwegianlanguageproject.model.Vocabulary;
+
+import java.util.List;
+
 public interface VocabularyService {
+
+    List<Vocabulary> getAllVocabularies();
+
+    Vocabulary saveVocabulary(Vocabulary vocabulary);
+
+    Vocabulary updateVocabulary(Vocabulary vocabulary);
+
+    Vocabulary getVocabularyById(Long id);
+
+    void deleteById(Long id);
 }
--- src/main/java/pl/milenamrugala/norwegianlanguageproject/service/VocabularyServiceImpl.java
+++ src/main/java/pl/milenamrugala/norwegianlanguageproject/service/VocabularyServiceImpl.java
@@ -1,7 +1,43 @@
 package pl.milenamrugala.norwegianlanguageproject.service;
 
 import org.springframework.stereotype.Service;
+import pl.milenamrugala.norwegianlanguageproject.model.Vocabulary;
+import pl.milenamrugala.norwegianlanguageproject.repository.VocabularyRepository;
+
+import java.util.List;
 
 @Service
-public class VocabularyServiceImpl {
+public class VocabularyServiceImpl implements VocabularyService {
+
+    private VocabularyRepository vocabularyRepository;
+
+    public VocabularyServiceImpl(VocabularyRepository vocabularyRepository) {
+        this.vocabularyRepository = vocabularyRepository;
+    }
+
+    @Override
+    public List<Vocabulary> getAllVocabularies() {
+        return vocabularyRepository.findAll();
+    }
+
+    @Override
+    public Vocabulary saveVocabulary(Vocabulary vocabulary) {
+        return vocabularyRepository.save(vocabulary);
+    }
+
+    @Override
+    public Vocabulary updateVocabulary(Vocabulary vocabulary) {
+        return vocabularyRepository.save(vocabulary);
+    }
+
+    @Override
+    public Vocabulary getVocabularyById(Long id) {
+        return vocabularyRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vocabularyRepository.deleteById(id);
+
+    }
 }
